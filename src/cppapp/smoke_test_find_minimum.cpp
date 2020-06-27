c79d775834c2a3c232631660fb1ca8a2683ea059
lktrgl <32882989+lktrgl@users.noreply.github.com>
2020-06-27 13:20:15 +0300

    code clean up

--- src/cppapp/smoke_test_find_minimum.cpp
+++ src/cppapp/smoke_test_find_minimum.cpp
@@ -8,8 +8,12 @@
 #include <cassert>
 #include <cmath>
 
+namespace
+{
+
 // smoke test for the noptim::find_minimum_dichotomie<>(.)
-void smoke_test_find_minimum_dichotomie()
+template<noptim::find_minimum_method METHOD_ENUM>
+void smoke_test_find_minimum_X ( size_t const iter_count1, size_t const iter_count2 )
 {
   {
     auto my_f = [] ( double x )->double
@@ -25,10 +29,10 @@ void smoke_test_find_minimum_dichotomie()
 
     noptim::find_minimum_t stat;
     auto const x_min =
-      noptim::find_minimum<noptim::find_minimum_method::dichotomie> ( xa, xb, eps, my_f, &stat );
+      noptim::find_minimum<METHOD_ENUM> ( xa, xb, eps, my_f, &stat );
 
     assert ( fabs ( x_min - expected_x_min ) <= eps );
-    assert ( 21 == stat.funct_invocation_count );
+    assert ( iter_count1 == stat.funct_invocation_count );
   }
 
   {
@@ -47,57 +51,25 @@ void smoke_test_find_minimum_dichotomie()
 
     noptim::find_minimum_t stat;
     auto const x_min =
-      noptim::find_minimum<noptim::find_minimum_method::dichotomie> ( xa, xb, eps, my_f, &stat );
+      noptim::find_minimum<METHOD_ENUM> ( xa, xb, eps, my_f, &stat );
 
     assert ( fabs ( x_min - expected_x_min ) <= eps );
-    assert ( 23 == stat.funct_invocation_count );
+    assert ( iter_count2 == stat.funct_invocation_count );
   }
+}
+
+} // namespace anonymous
+
+// smoke test for the noptim::find_minimum_dichotomie<>(.)
+void smoke_test_find_minimum_dichotomie()
+{
+  smoke_test_find_minimum_X<noptim::find_minimum_method::dichotomie> ( 21, 23 );
 }
 
 // smoke test for the noptim::find_minimum_gold_ratio<>(.)
 void smoke_test_find_minimum_gold_ratio()
 {
-  {
-    auto my_f = [] ( double x )->double
-    {
-      return ( x - 1.0 ) * ( x - 1.0 );
-    };
-
-    auto const xa = -1.0;
-    auto const xb = 2.0;
-    auto const eps = 0.01;
-
-    auto const expected_x_min = 1.0;
-
-    noptim::find_minimum_t stat;
-    auto const x_min =
-      noptim::find_minimum<noptim::find_minimum_method::gold_ratio> ( xa, xb, eps, my_f, &stat );
-
-    assert ( fabs ( x_min - expected_x_min ) <= eps );
-    assert ( 14 == stat.funct_invocation_count );
-  }
-
-  {
-    constexpr auto const d = 10.0;
-
-    auto my_f = [] ( double x )->double
-    {
-      return - ( d* d * x - x* x * x );
-    };
-
-    auto const xa = 0.;
-    auto const xb = d;
-    auto const eps = 0.01;
-
-    constexpr auto const expected_x_min = d / sqrt ( 3.0 );
-
-    noptim::find_minimum_t stat;
-    auto const x_min =
-      noptim::find_minimum<noptim::find_minimum_method::gold_ratio> ( xa, xb, eps, my_f, &stat );
-
-    assert ( fabs ( x_min - expected_x_min ) <= eps );
-    assert ( 17 == stat.funct_invocation_count );
-  }
+  smoke_test_find_minimum_X<noptim::find_minimum_method::gold_ratio> ( 14, 17 );
 }
 
 // smoke test for the noptim::quick_descent<> class
