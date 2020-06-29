082814b50238d012a72f95d8db6b38b2a992112c
lktrgl <32882989+lktrgl@users.noreply.github.com>
2020-06-29 13:59:33 +0300

    the integration smoke test is updated

--- src/cppapp/smoke_test_integral.cpp
+++ src/cppapp/smoke_test_integral.cpp
@@ -17,17 +17,19 @@ void smoke_test_X ( ARG_TYPE const h_in, ARG_TYPE const eps_in )
   using my_ret_type_t = typename my_integral::ret_type_t;
   using my_funct_arg_t = typename my_integral::funct_arg_t;
 
+  constexpr my_funct_arg_t const A = 3.0;
+
   auto my_funct = [] ( double const & t )
   {
-    return exp ( -t * t );
+    return A * cos ( t );
   };
 
   my_funct_arg_t const h = h_in;
-  my_funct_arg_t const a = -10.0;
-  my_funct_arg_t const b = 10.0;
+  constexpr my_funct_arg_t const a = 0.0;
+  constexpr my_funct_arg_t const b = M_PI / 2.0;
   my_funct_arg_t const eps = eps_in;
 
-  my_ret_type_t const expected_integral_value = sqrt ( M_PI );
+  constexpr my_ret_type_t const expected_integral_value = A * sin ( b );
 
   my_integral integr ( h, my_funct );
 
@@ -40,7 +42,7 @@ void smoke_test_X ( ARG_TYPE const h_in, ARG_TYPE const eps_in )
 
 void smoke_test_rectangle()
 {
-  smoke_test_X<integral::integral_method::rectangle, double, double> ( 0.1, 0.001 );
+  smoke_test_X<integral::integral_method::rectangle, double, double> ( 0.0006, 0.001 );
 }
 
 void smoke_test_trapezoid()
